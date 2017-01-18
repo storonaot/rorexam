e5a41bf29323eb1aaf63a68b93053310d87e04f7
storonaot <storonaot@gmail.com>
2017-01-18 22:49:30 +0300

    added locales

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Rorexam
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    # config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.i18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.fallbacks = true
+    config.i18n.fallbacks = [:en]
+    config.i18n.enforce_available_locales = true
   end
 end
